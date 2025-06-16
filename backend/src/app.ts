c024b3823a85a1d7d25a5b4f0a593ad10406d6b0
Jose Quintana <joseguardoq@gmail.com>
2025-06-16 01:59:22 -0500

    Add cors origin for deployed front-end

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,30 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import connectDB from './config/db.js'
 import customerRoutes from './routes/customerUserRoutes.js'
 import adminRoutes from './routes/adminUserRoutes.js'
 import { errorHandler } from './middlewares/errorMiddleware.js'
-import connectDB from './config/db.js'
-import cookieParser from 'cookie-parser'
 import productsRoutes from './routes/productsRoutes.js'
 import cartRoutes from './routes/cartRoutes.js'
-import dotenv from 'dotenv'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+const allowedOrigins = ['http://localhost:5173', process.env.CLIENT_URL]
 const app = express()
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+    callback(new Error('Not allowed by CORS'))
+  },
+  credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false}))
