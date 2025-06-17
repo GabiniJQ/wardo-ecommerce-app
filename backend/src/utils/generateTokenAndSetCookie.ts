efaba4c306a20b7fa716ee00eb46b78153e6f445
Jose Quintana <joseguardoq@gmail.com>
2025-06-17 17:26:51 -0500

    Change cookies options for cross-site requests

--- backend/src/utils/generateTokenAndSetCookie.ts
+++ backend/src/utils/generateTokenAndSetCookie.ts
@@ -11,7 +11,7 @@ export const generateTokenAndSetCookie = (res: Response, userId: mongoose.Types.
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
